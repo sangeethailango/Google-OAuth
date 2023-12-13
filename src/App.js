be38a8bb4775ff0d296805a5a02f5da6eee8f481
sangeetha <sangeethailango21@gmail.com>
2023-12-13 14:39:58 +0530

    Sign in page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
+    return (
+			<div>
+					<h2>Login with your google</h2>
+					<br />
+					<br />
+					<GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+			</div>
+    )
 }
-
 export default App;
