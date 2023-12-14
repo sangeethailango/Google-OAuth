78368f2f7a5b7996504d58c6482941cddf854e0f
sangeetha <sangeethailango21@gmail.com>
2023-12-14 16:59:59 +0530

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
+import raw from './encrypted_emails.txt';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AuthenticatedContent from './components/authenticatedcontent.js';
+import Overlay from "./components/Overlay";
+import NonAuthenticatedContent from './components/non-authenticated-contented.js';
 
 function App() {
-    const responseMessage = (response) => {
-        console.log(response);
+  const [decryptedEmails, setDecryptedEmails] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isToken ,setToken] = useState(false)
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(raw);
+        const text = await response.text();
+        const lines = text.split('\n');
+
+        const emails = lines.map((line) => {
+          try {
+            const decoded = jwtDecode(line);
+            return decoded.email;
+          } catch (error) {
+            console.error('Invalid token:', line);
+            return null;
+          }
+        });
+
+        const validEmails = emails.filter((email) => email !== null);
+
+        setDecryptedEmails(validEmails);
+      } catch (error) {
+        console.error('Error reading or decoding file:', error);
+      }
     };
-    const errorMessage = (error) => {
-        console.log(error);
-    };
-    return (
-			<div>
-					<h2>Login with your google</h2>
-					<br />
-					<br />
-					<GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-			</div>
-    )
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const toggleOverlayAfterDelay = setTimeout(() => {
+      setShowOverlay(false);
+    }, 10000);
+
+    return () => clearTimeout(toggleOverlayAfterDelay);
+  }, []);
+
+  const responseMessage = (response) => {
+    const token = response?.credential;
+		console.log('Token: ',token)
+
+		if (token !== null && token !== undefined) {
+			setToken(true);
+		} else {
+			setToken(false);
+		}
+		
+    if (token) {
+      const decoded = jwtDecode(token);
+			console.log( 'Decode: ' ,decoded)
+
+      if (decryptedEmails.includes(decoded.email)) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    }
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? (
+                <AuthenticatedContent />
+              ) : (
+								isToken ? (
+                <NonAuthenticatedContent />
+              ) : (
+								<Overlay isOpen={showOverlay} onClose={() => setShowOverlay(false)}>
+								  <GoogleLogin onSuccess={responseMessage} />
+							  </Overlay>
+							)
+						)
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
+
---
+++ src/components/authenticatedcontent.js
@@ -0,0 +1,12 @@
+import React, {Fragment, useState, useEffect} from 'react';
+
+const AuthenticatedContent = () => {
+  return (
+    
+    <div>
+      <h2>Hello World!!!!</h2>
+    </div>
+  );
+};
+
+export default AuthenticatedContent;
